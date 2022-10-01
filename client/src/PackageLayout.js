aef8892d392c6fba353525a2442ef76469e8123e
Jeffrey Lee <jeffrey@JeffreysMacBook.hitronhub.home>
2022-10-01 10:01:39 -0400

    react router check

--- client/src/PackageLayout.js
+++ client/src/PackageLayout.js
@@ -1,16 +1,25 @@
-import { Link, Outlet } from "react-router-dom";
-
+import { Link, Outlet, useSearchParams } from "react-router-dom";
+import { useState } from "react";
 export function PackageLayout() {
+  const [searchParams, setSearchParams] = useSearchParams({n:1})
+
+  const number = searchParams.get("n")
   return (
     <>
-    <Link to="/package/1">Package 1</Link>
-    <br />
-    <Link to="/package/2">Package 2</Link>
-    <br />
-    <Link to="/package/new">New Package</Link>
-    <br />
-    <Outlet />
+      <Link to="/package/1">Package 1</Link>
+      <br />
+      <Link to="/package/2">Package 2</Link>
+      <br />
+      <Link to={`/package/${number}`}>Package {number}</Link>
+      <br />
+      <Link to="/package/new">New Package</Link>
+      <br />
+      <Outlet />
+      <input
+        type="number"
+        value={number}
+        onChange={e => setSearchParams({n: e.target.value})}
+      />
     </>
-  )
-    
+  );
 }
