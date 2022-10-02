c71bc9ed33330b32cb11afdfc320d3226ece7023
Jeffrey Lee <jeffrey@JeffreysMacBook.hitronhub.home>
2022-10-02 12:13:23 -0400

    button component added and added to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,11 +3,16 @@ import { CategoryFilter } from "../components/CategoryFilter";
 import { PackageList } from "../components/PackageList";
 import { ReactCalendar } from "../components/Calendar";
 import 'react-calendar/dist/Calendar.css';
+import Button from "../components/Button";
+
 export function Home() {
   return (
     <>
     <CategoryFilter /> 
       <ReactCalendar />
+      <Button 
+      onClick={()=> console.log('Button works!')}
+      />
       <PackageList />;
     </>
   );
