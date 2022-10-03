f97c209fef4019b7c2aac2a60c1359fabe75c3c3
paige-clark <paigeclarkillustration@gmail.com>
2022-10-03 14:46:24 -0400

    fix broken formatting

--- client/src/pages/Package.js
+++ client/src/pages/Package.js
@@ -1,15 +1,15 @@
-import './styles/Package.scss'
-import { useParams } from 'react-router-dom'
-import { packages } from '../mock_data/packages'
-import packageImg from '../assets/product_page.png'
-import { ReactCalendar } from '../components/Calendar'
-import { BookingBox } from '../components/BookingBox'
-import { PackageInfoCard } from '../components/PackageInfoCard'
-import 'react-calendar/dist/Calendar.css'
+import "./styles/Package.scss";
+import { useParams } from "react-router-dom";
+import { packages } from "../mock_data/packages";
+import packageImg from "../assets/product_page.png";
+import { ReactCalendar } from "../components/Calendar";
+import { BookingBox } from "../components/BookingBox";
+import { PackageInfoCard } from "../components/PackageInfoCard";
+import "react-calendar/dist/Calendar.css";
 
 export function Package() {
-  const { id } = useParams()
-  const currentPackage = packages.filter((pack) => pack.id === Number(id))
+  const { id } = useParams();
+  const currentPackage = packages.filter((pack) => pack.id === Number(id));
 
   // <PackageListItem
   //       key={packageItem.id}
@@ -23,32 +23,36 @@ export function Package() {
   //     />
 
   return (
-    <>
-      <h1>This package type is: {id}</h1>
-
-      <div>
-        <img src={packageImg} alt='img' width='100px' />
-        {/* this should be a component */}
-        <PackageInfoCard
-          tent={currentPackage[0].tent_description}
-          bags={currentPackage[0].bags_description}
-          lantern={currentPackage[0].lantern_description}
-          cooking={currentPackage[0].cooking_description}
-        />
+    <div className="Package">
+      <div className="package-top">
+        <div>
+          <img src={packageImg} alt="img" className="package-img" />
+        </div>
+        <div className="card">
+          <PackageInfoCard
+            tent={currentPackage[0].tent_description}
+            bags={currentPackage[0].bags_description}
+            lantern={currentPackage[0].lantern_description}
+            cooking={currentPackage[0].cooking_description}
+          />
+        </div>
       </div>
-      <div>
-        THIS CATEGORY IS: {currentPackage[0].category}
-        <br />
-        THIS OWNER IS: {currentPackage[0].user_id}
-        <br />
-        {currentPackage[0].description}
-      </div>
+      <div className="package-bottom">
+        <div>
+          <h2>
+            {currentPackage[0].category} Person Package
+            <br />
+            Gear owned by user {currentPackage[0].user_id}
+          </h2>
+          {currentPackage[0].description}
+        </div>
       <BookingBox
         price={currentPackage[0].price}
         packageID={currentPackage[0].id}
       />
       {/* <ReactCalendar /> */}
       {/* availability calendar left here (stretch) */}
-    </>
-  )
+      </div>
+    </div>
+  );
 }
