ebcd69b7fb8c16aeb556d3d6160e8cd4ba88f6e8
Jeffrey Lee <jeffrey@JeffreysMacBook.hitronhub.home>
2022-10-03 00:16:21 -0400

    Availabilty range checker started

--- client/src/components/AvailabilityFunc.js
+++ client/src/components/AvailabilityFunc.js
@@ -1,19 +1,47 @@
-const addDays = function (days) {
-  var date = new Date(this.valueOf());
-  date.setDate(date.getDate() + days);
-  return date;
+//could be renamed to Date Range overlap check
+const dateRangeOverlaps = function (a_start, a_end, b_start, b_end) {
+  if (a_start <= b_start && b_start <= a_end) return true; // b starts in a
+  if (a_start <= b_end && b_end <= a_end) return true; // b ends in a
+  if (b_start < a_start && a_end < b_end) return true; // a in b
+  return false;
 };
-
-//October 7, 2022
-//October 10, 2022
-const getDates = function (startDate, stopDate) {
-  var dateArray = new Array();
-  var currentDate = startDate;
-  while (currentDate <= stopDate) {
-    dateArray.push(new Date(currentDate));
-    currentDate = currentDate.addDays(1);
+const multipleDateRangeOverlaps = function () {
+  var i, j;
+  if (arguments.length % 2 !== 0)
+    throw new TypeError("Arguments length must be a multiple of 2");
+  for (i = 0; i < arguments.length - 2; i += 2) {
+    for (j = i + 2; j < arguments.length; j += 2) {
+      if (
+        dateRangeOverlaps(
+          arguments[i],
+          arguments[i + 1],
+          arguments[j],
+          arguments[j + 1]
+        )
+      )
+        return true;
+    }
   }
-  return dateArray;
+  return false;
 };
 
-console.log(getDates("October 7 2022", "October 10 2022" ))
+
+//overlap
+console.log(
+  multipleDateRangeOverlaps(
+    "2022-10-07T04:00:00.000Z",
+    "2022-10-08T04:00:00.000Z",
+    "2022-10-07T04:00:00.000Z",
+    "2022-10-10T04:00:00.000Z"
+  )
+);
+
+//does not overlap
+console.log(
+  multipleDateRangeOverlaps(
+    "2022-10-07T04:00:00.000Z",
+    "2022-10-08T04:00:00.000Z",
+    "2022-10-09T04:00:00.000Z",
+    "2022-10-10T04:00:00.000Z"
+  )
+);
