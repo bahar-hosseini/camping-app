78c57c86785a1c2324d3799eab22c8f2bcea519e
Jeffrey Lee <jeffrey@JeffreysMacBook.hitronhub.home>
2022-10-03 12:21:02 -0400

    check booking range avail

--- client/src/components/AvailabilityFunc.js
+++ client/src/components/AvailabilityFunc.js
@@ -1,3 +1,29 @@
+/* eslint-disable eqeqeq */
+//import { bookings } from "../mock_data/bookings";
+const allBookings = require("../mock_data/bookings");
+// const parsedAllBookings = JSON.stringify(allBookings)
+// console.log(parsedAllBookings)
+const bookingForSpecificPackage = function (packageID) {
+  const listOfBookings = allBookings.bookings.filter(
+    (book) => book.package_id == packageID
+  );
+
+  return listOfBookings;
+};
+// console.log(bookingForSpecificPackage(4))
+
+const allDates = function (arry) {
+  let dates = [];
+  for (let items of arry) {
+    // console.log(items)
+    dates.push(items.start_date);
+    dates.push(items.end_date);
+  }
+  return dates;
+};
+
+
+
 //could be renamed to Date Range overlap check
 const dateRangeOverlaps = function (a_start, a_end, b_start, b_end) {
   if (a_start <= b_start && b_start <= a_end) return true; // b starts in a
@@ -5,42 +31,46 @@ const dateRangeOverlaps = function (a_start, a_end, b_start, b_end) {
   if (b_start < a_start && a_end < b_end) return true; // a in b
   return false;
 };
-const multipleDateRangeOverlaps = function () {
+const multipleDateRangeOverlaps = function (arry) {
   var i, j;
-  if (arguments.length % 2 !== 0)
+  if (arry.length % 2 !== 0)
     throw new TypeError("Arguments length must be a multiple of 2");
 
-  for (i = 0; i < arguments.length - 2; i += 2) {  
+  for (i = 0; i < arry.length - 2; i += 2) {
     //console.log('this is arg i: ', arguments[i])
-    for (j = i + 2; j < arguments.length; j += 2) {
+    for (j = i + 2; j < arry.length; j += 2) {
       //console.log('this is arg j: ', arguments[j])
-      if (
-        dateRangeOverlaps(
-          arguments[i],
-          arguments[i + 1],
-          arguments[j],
-          arguments[j + 1]
-        )
-      )
+      if (dateRangeOverlaps(arry[i], arry[i + 1], arry[j], arry[j + 1]))
         return true;
     }
   }
   return false;
 };
+console.log(allDates(bookingForSpecificPackage(4)))
+console.log(multipleDateRangeOverlaps(allDates(bookingForSpecificPackage(4))))
+
+
 
 
 //overlap
-console.log(
-  multipleDateRangeOverlaps(
-    "2022-10-07T04:00:00.000Z",
-    "2022-10-08T04:00:00.000Z",
-    "2022-10-09T04:00:00.000Z",
-    "2022-10-10T04:00:00.000Z",
-    "2022-10-10T04:00:00.000Z",
-    "2022-10-12T04:00:00.000Z"
-  )  
-);
+// console.log(
+//   multipleDateRangeOverlaps(
+//     "2022-10-07T04:00:00.000Z",
+//     "2022-10-08T04:00:00.000Z",
+//     "2022-10-09T04:00:00.000Z",
+//     "2022-10-10T04:00:00.000Z",
+//     "2022-10-10T04:00:00.000Z",
+//     "2022-10-12T04:00:00.000Z"
+//   )
+// );
 
+// const testData = [
+//   "2022-10-07T04:00:00.000Z",
+//   "2022-10-08T04:00:00.000Z",
+//   "2022-10-09T04:00:00.000Z",
+//   "2022-10-10T04:00:00.000Z",
+// ];
+// console.log(multipleDateRangeOverlaps(testData))
 //does not overlap
 // console.log(
 //   multipleDateRangeOverlaps(
@@ -51,17 +81,9 @@ console.log(
 //   )
 // );
 
-
-
-// 1. add SD and ED to booking 
-// 2. filter all bookings for one package 
+// 1. add SD and ED to booking **DONE
+// 2. filter all bookings for one package  **DONE
 // 3. get all SD and ED for that one package ( array)
 // 4. get user's requested SD and ED (1 SD and ED )
-// 5.use the function to check overlap 
+// 5.use the function to check overlap
 // 6.tada!!
-
-
-
-
-
-
