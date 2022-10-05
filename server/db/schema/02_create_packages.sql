d6b3c49e16da72a24f2372a2354b9d0470eac683
paige-clark <paigeclarkillustration@gmail.com>
2022-10-05 15:48:51 -0400

    update schema

--- server/db/schema/02_create_packages.sql
+++ server/db/schema/02_create_packages.sql
@@ -3,14 +3,16 @@ DROP TABLE IF EXISTS packages CASCADE;
 -- CREATE URLS
 CREATE TABLE packages (
   id SERIAL PRIMARY KEY NOT NULL,
-  image character varying(255) NOT NULL,
+  home_img TEXT NOT NULL,
+  package_img TEXT NOT NULL,
+  booking_img TEXT NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   price INTEGER  NOT NULL DEFAULT 0,
   category INTEGER  NOT NULL DEFAULT 0,
- description character varying(500) NOT NULL,
- tent_description character varying(255) NOT NULL,
- bags_description character varying(255) NOT NULL,
- lantern_description character varying(255) NOT NULL,
- cooking_description character varying(255) NOT NULL,
- location INTEGER  NOT NULL DEFAULT 0
+  description character varying(500) NOT NULL,
+  tent_description character varying(255) NOT NULL,
+  bags_description character varying(255) NOT NULL,
+  lantern_description character varying(255) NOT NULL,
+  cooking_description character varying(255) NOT NULL,
+  location INTEGER  NOT NULL DEFAULT 0
 );
