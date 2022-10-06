8b1cef0cb7e7f2ccf5bc0eedd50e7eeb13a62a74
Jeffrey Lee <jeffrey@JeffreysMacBook.hitronhub.home>
2022-10-06 18:19:54 -0400

    home debuged

--- client/src/providers/SearchProvider.js
+++ client/src/providers/SearchProvider.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useRef } from "react";
 // import DatePicker from "react-date-picker";
 // Create a Context
 import { differenceInDays } from "date-fns";
@@ -16,33 +16,32 @@ export default function SearchProvider(props) {
   const [packages, setPackages] = useState([]);
 
   const [loading, setLoading] = useState(false);
-
+  const isLoadedRef = useRef(false);
   useEffect(() => {
-    setLoading(true);
+    // const abortCont = new AbortController();
     axios
-      .get("/api/packages/filter", { params: { category,endDate, startDate } })
+      .get("/api/packages")
+      // .then(()=> setDateRange(startDate, endDate))
+      .then((res) => {
+        setPackages(res.data.data.rows);
+        isLoadedRef.current = true;
+      });
+    return () => setPackages([]);
+  }, []);
+
+  
+  useEffect(() => {
+    if(isLoadedRef.current===false) return
+    axios
+      .get("/api/packages/filter", { params: { category, endDate, startDate } })
       // .then((res) => console.log(res.data.data))
       .then((res) => {
         //console.log(res.data.data,'running@@@@@@')
         setPackages(res.data.data);
-        setLoading(false);
       });
-      // return ()=>console.log('cleanup')
-      return ()=>setPackages([]);
+    // return ()=>console.log('cleanup')
+    return () => setPackages([]);
   }, [category, endDate, startDate]);
-
-
-  useEffect(() => {
-    // const abortCont = new AbortController();
-    setLoading(true);
-    axios.get("/api/packages")
-    // .then(()=> setDateRange(startDate, endDate))
-    .then((res) => { setPackages(res.data.data.rows)});
-    return ()=>setPackages([]); 
-  }, []);
-
-    
-
 
   const diff = differenceInDays(endDate, startDate);
   //todo: set diff as a state
