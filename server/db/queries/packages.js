df59b1a6077d6a38f3e65447ee4c2165226fdd66
Bahar Hosseini <bahar.hosseini@gmail.com>
2022-10-11 14:02:58 -0400

    fix minor bugs

--- server/db/queries/packages.js
+++ server/db/queries/packages.js
@@ -18,8 +18,8 @@ const getPackages = (eachQuery) => {
 const getPackage = (packagetId) => {
   return db
     .query(
-      `SELECT * , users.name AS name FROM packages 
-    JOIN users ON users.id = user_id
+      `SELECT packages.* , users.name AS name FROM packages 
+    JOIN users ON packages
     WHERE packages.id = $1;`,
       [packagetId]
     )
